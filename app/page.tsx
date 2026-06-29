589f4a7287b4762d88ce59e1de58dddd396b8579
ged-bmu <trhxhien@gmail.com>
2026-06-29 10:36:37 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -20,17 +20,27 @@ useEffect(() => {
 }, []);
 
 async function checkUser() {
-const {
-data: { user },
-} = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-if (!user) {
-  window.location.href = "/introduce";
-  return;
-}
+  if (!user) {
+    window.location.href = "/introduce";
+    return;
+  }
 
-setLoading(false);
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("role")
+    .eq("id", user.id)
+    .single();
 
+  if (profile?.role === "admin") {
+    window.location.href = "/admin";
+    return;
+  }
+
+  setLoading(false);
 }
 
 const folders = [
