13662b3adf75e999e71e0e9762fd683f08193de5
ged-bmu <trhxhien@gmail.com>
2026-08-05 23:13:15 +0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "./mobile.css";
 import RegisterSW from "./components/RegisterSW";
+import "./globals.css";
+import "./mobile-common.css";
+import "./mobile-home.css";
+import "./mobile-introduce.css";
+import "./mobile-chat.css";
+import "./mobile-profile.css";
+import "./mobile-admin.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
